9d4ec1f0befb6cd315fa4a0d6a1831d77b5b1836
İrem <irempakyurek1@gmail.com>
2019-08-28 21:35:03 +0300

    first day

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-post-create',
+  templateUrl : './post-create.component.html',
+  styleUrls : ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
+
+  onAddPost(){
+    const post = {
+      title : this.enteredTitle,
+      content: this.enteredContent
+    };
+  }
+}
